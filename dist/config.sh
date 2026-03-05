10258774199d20891dd3c0e46eae1a79fa7bbc47
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 16:15:51 +0800

    Update config.sh

--- dist/config.sh
+++ dist/config.sh
@@ -1,1 +1,7 @@
 
+{ [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf || true; }
+sudo systemctl daemon-reload && sudo usermod -aG video,audio,lp,scanner "$(logname)"
+{ [[ "$IS_LAPTOP" == "1" ]] && sudo systemctl enable tlp; }
+{ [[ "$IS_LAPTOP" == "1" && "$CPU_VENDOR" =~ "GenuineIntel" ]] && sudo systemctl enable thermald; }
+sudo systemctl enable lightdm bluetooth NetworkManager
+sudo -u "$USER0" systemctl --user enable --now pipewire pipewire-pulse wireplumber
