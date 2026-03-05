03e860ba6b2bb390cf8546bf5b5eaf143f33a68a
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 21:55:03 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,10 +5,10 @@ URL="https://raw.githubusercontent.com/Kaiserrrrrr/LXI/main/dist"
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    echo -e "\e[32m[✓] LXI Update Complete.\e[0m" && sync
+    echo -e "\e[32m[✓] Aqtive Update Complete.\e[0m" && sync
 else
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/config.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    echo -e "\e[33m[!] LXI Installation Complete. Rebooting in 5s to apply changes...\e[0m" && sync && sleep 3 && sudo reboot
+    echo -e "\e[33m[!] Aqtive Installation Complete. Rebooting in 5s to apply changes...\e[0m" && sync && sleep 3 && sudo reboot
 fi
