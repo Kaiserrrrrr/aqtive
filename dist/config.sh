844218817f3a2e3b571a2e76884c2acd4f7b775f
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 16:16:19 +0800

    Update config.sh

--- dist/config.sh
+++ dist/config.sh
@@ -1,3 +1,5 @@
+CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo)
+IS_LAPTOP=$([[ -d /sys/class/power_supply/BAT0 || -d /sys/class/input/mouse0 ]] && echo 1 || echo 0)
 
 { [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf || true; }
 sudo systemctl daemon-reload && sudo usermod -aG video,audio,lp,scanner "$(logname)"
