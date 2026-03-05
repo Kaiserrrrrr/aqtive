53fd2604275192b7b8378bda8c1aff0fa78f7759
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 16:16:59 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ URL="https://raw.githubusercontent.com/Kaiserrrrrr/LXI/main/dist"
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    echo -e "\e[32m[✓] LXI Update Complete.\e[0m" && sync && sleep 3 && clear
+    echo -e "\e[32m[✓] LXI Update Complete.\e[0m" && sync
 else
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/config.sh" | sh && \
