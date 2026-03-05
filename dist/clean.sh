6bdce6b2f385d0034aa4b06fdf983e6c3ec5b8db
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 15:23:48 +0800

    Update clean.sh

--- dist/clean.sh
+++ dist/clean.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo journalctl --vacuum-size=5M >/dev/null 2>&1
+sudo journalctl --vacuum-size=5M 
 sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=5M/' /etc/systemd/journald.conf
 sudo sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=5M/' /etc/systemd/journald.conf
-sudo systemctl restart systemd-journald >/dev/null 2>&1
-find /var/log -type f -exec truncate -s 0 {} + >/dev/null 2>&1
+sudo systemctl restart systemd-journald 
+find /var/log -type f -exec truncate -s 0 {} + 
 
 orphans=$(pacman -Qtdq)
 if [[ -n "$orphans" ]]; then
-    sudo pacman -Rns $orphans --noconfirm >/dev/null 2>&1
+    sudo pacman -Rns $orphans --noconfirm 
 fi
 
 hw_data=$(lspci -k; lsusb)
@@ -23,11 +23,11 @@ if [[ -n "$hw_data" ]]; then
         fi
         
         if ! echo "$hw_data" | grep -wiq "$vendor"; then
-            sudo pacman -Rdd --noconfirm "$pkg" >/dev/null 2>&1
+            sudo pacman -Rdd --noconfirm "$pkg" 
         fi
     done
 fi
 
-sudo pacman -Scc --noconfirm >/dev/null 2>&1
+sudo pacman -Scc --noconfirm 
 rm -rf ~/.cache/thumbnails/* ~/.cache/fontconfig/* ~/.local/share/Trash/*
 echo -e "\e[32m[✓] Cleanup Complete\e[0m"
