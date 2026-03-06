4ed6e0859694f56365cf20c3638fdd51b4151f07
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-06 15:25:10 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 URL="https://raw.githubusercontent.com/Kaiserrrrrr/aqtive/main/dist"
-reboot_now() { read -p "Reboot Now? [Y/n]: " res; case "$res" in [Yy]*) sudo reboot ;; [Nn]*) exit 0 ;; *) exit 1 ;; esac; } 
 
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    sync && echo -n "[✓] Aqtive Update Complete. " && reboot_now
+    sync && echo -n "[✓] Aqtive Update Complete. "
+    read -p "Reboot now to apply changes? [Y/n] " res; case "$res" in [Yy]*) reboot ;; [Nn]*) echo "Reboot manually to apply changes." && return 0 ;; *) return 0 ;; esac
 else
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/config.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    sync && echo -n "[✓] Aqtive Installation Complete. " && reboot_now
+    sync && echo -n "[✓] Aqtive Installation Complete. "
+    read -p "Reboot now to apply changes? [Y/n] " res; case "$res" in [Yy]*) reboot ;; [Nn]*) echo "Reboot manually to apply changes." && return 0 ;; *) return 0 ;; esac
 fi
