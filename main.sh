fed300d47bf1196cbd5597fef874b8f81ca91005
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-07 02:06:17 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# { [ "$(id -u)" -ne 0 ] && echo "Error: This script must be run as root." >&2 && exit 1; } 
-reboot_now() {  read -p "Reboot now to apply changes? [Y/n] " res < /dev/tty; case "$res" in [Yy]*) reboot ;; [Nn]*) return 0 ;; *) return 0 ;; esac; }
-
 URL="https://raw.githubusercontent.com/Kaiserrrrrr/aqtive/main/dist"
+reboot_now() {  read -p "Reboot now to apply changes? [Y/n] " res < /dev/tty; case "$res" in [Yy]*) reboot ;; [Nn]*) return 0 ;; *) return 0 ;; esac; }
 
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
