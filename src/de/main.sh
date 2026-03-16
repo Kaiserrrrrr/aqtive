ef197c0ad4a9010aaf448380716aa4f4377459ef
Kaiser <97164185+kaiserrrrrr@users.noreply.github.com>
2026-03-16 16:49:22 +0800

    Update main.sh

--- src/de/main.sh
+++ src/de/main.sh
@@ -3,6 +3,9 @@
 sudo -v; clear
 
 URL="https://raw.githubusercontent.com/kaiserrrrrr/aqtive/main/src/de"
+DE="lxqt"
+
+# read -p ""
 
 PKG=$(curl -s $URL/$DE/install.txt)
 sudo pacman -Syu --noconfirm && sudo pacman $PKG --noconfirm && sudo pacman -Scc --noconfirm
